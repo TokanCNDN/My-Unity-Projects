499576f9e22f8a49d3efc89d65add587a82a0929
TokanCNDN <okii.onr@hotmail.com>
2023-07-06 18:23:09 +0300

    Hyper Casual

---
+++ GoldStack/Assets/Scripts/NodeMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeMovement : MonoBehaviour
+{
+    public Transform ConnectNode;
+    void Update()
+    {
+        transform.position = new Vector3(
+            Mathf.Lerp(transform.position.x,ConnectNode.position.x,Time.deltaTime*20),
+            ConnectNode.position.y,
+            ConnectNode.position.z +3
+            );
+    }
+}
